9ca49aade3baa9d2bf5e29fbcb07a4b937a4b0a2
black <joseb.twelve@gmail.com>
2022-08-25 16:06:56 -0500

    fix: deleted unnecesary comments and deleted nested loop

--- src/two_sum.py
+++ src/two_sum.py
@@ -6,29 +6,21 @@ def two_sum(
     num_list: List[int], expected_result: int
 ) -> Generator[tuple[int, int], None, None]:
     """
-    two_sum function to search two numbers into a list of numbers and
-    if the result of the addition operation between those two numbers
-    is equals to expected_result, then those numbers will be yield
-    from a generator:
-
-    > two_sum([2,5,6,1], 7)
-
-    > 2,5
-
-    > 6,1
+    two_sum function search two numbers and their sum must be expected result.
 
     Params:
         - num_list: List[int] | List of numbers
         - expected_result: int | A number and this will be the result of the sum
             operation between two different numbers from 'num_list' var
+
     Returns:
         - num_list_i: int | First value found to the sum
-        - num_list_j: int | Second value found to the sum
+        - complement: int | Second value found to the sum
     """
-    # look up the first element in the list
+    complements = []
     for i in range(len(num_list) - 1):
-        # look up other element in the list
-        for j in range(i + 1, len(num_list)):
-            # if these two elemets sum to expected_result, yield the pair elements
-            if num_list[i] + num_list[j] == expected_result:
-                yield num_list[i], num_list[j]
+        complement = expected_result - num_list[i]
+        if complement in complements:
+            yield num_list[i], complement
+        else:
+            complements.append(num_list[i])
