d3e13146a7abce335ca46f9297d0ba73516b7857
black <joseb.twelve@gmail.com>
2022-08-25 16:08:36 -0500

    feat: added new tests

--- src/test_two_sum.py
+++ src/test_two_sum.py
@@ -1,13 +1,30 @@
 from .two_sum import two_sum
 
 
-def test_two_sum():
+def test_two_sum_if_a_and_b_sum_to_expected():
+    num_list = [5, 2, 6, 1, 4, 3]
+    expected_result = 7
+    generator = two_sum(num_list, expected_result)
+    a, b = next(generator)
+
+    assert a + b == expected_result
+
+
+def test_two_sum_if_x_and_y_sum_to_expected():
+    num_list = [5, 2, 6, 1, 4, 3]
+    expected_result = 7
+    generator = two_sum(num_list, expected_result)
+    _, _ = next(generator)
+    x, y = next(generator)
+
+    assert x + y == expected_result
+
+
+def test_two_sum_if_returned_values_are_diferents():
     num_list = [5, 2, 6, 1, 4, 3]
     expected_result = 7
     generator = two_sum(num_list, expected_result)
     a, b = next(generator)
     x, y = next(generator)
 
-    assert a + b == expected_result
-    assert x + y == expected_result
     assert not x == a and not y == b
--- src/test_utils.py
+++ src/test_utils.py
@@ -1,8 +1,18 @@
 from .utils import load_cli_args
+import pytest
 
-def test_load_cli_args():
-    io_args = ['app.py', '5,2,6,1,4,3', '12']
+
+def test_load_cli_args_loads_successfully():
+    io_args = ["app.py", "5,2,6,1,4,3", "12"]
     num_list, expected_result = load_cli_args(io_args)
-    assert num_list == [5,2,6,1,4,3]
+    assert num_list == [5, 2, 6, 1, 4, 3]
     assert expected_result == 12
     assert type(expected_result) is int
+
+
+def test_load_cli_args_loads_fail():
+    io_args = ["app.py"]
+    expected_failure = "<ExceptionInfo ValueError('not enough values to unpack (expected 2, got 0)') tblen=2>"
+    with pytest.raises(ValueError) as value_error:
+        load_cli_args(io_args)
+    assert str(value_error) == expected_failure
