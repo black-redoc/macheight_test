0eeb29ac717f4d388cb438ee3b7dc02c9a6b880e
black <joseb.twelve@gmail.com>
2022-08-25 16:08:01 -0500

    fix: fixed slice and changed long comments to short comments

--- src/utils.py
+++ src/utils.py
@@ -4,20 +4,19 @@ from typing import List
 
 def load_cli_args(io_args: List[str]) -> tuple[List[int], int]:
     """
-    load_cli_args function to load I/O args from the command line and map them to
-    the expected output, that output is a tuple with a List of integers and an integer.
+    load_cli_args function loads args from command line and map it to a list and to an integer.
+
     Params:
         - io_args: List[str] | I/O args loaded when the app is called from the terminal
+
     Returns:
         - num_list: List[int] | List of numbers
         - expected_result: int | A number
     """
-    # split the io_args list in two strings
-    num_list, expected_result = io_args[1:]
-    # split the num_list string into a list of chars separating each value with a ','
+
+    num_list, expected_result = io_args[1:3]
     num_list = num_list.split(",")
-    # map each value from the mum_list to an integer
     num_list = list(map(lambda x: int(x), num_list))
-    # cast the expected value to an integer
+
     expected_result = int(expected_result)
     return num_list, expected_result
