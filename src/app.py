0aa1110d4ec97bd3ad8b8b525c3935b66234e2ad
black <joseb.twelve@gmail.com>
2022-08-25 16:09:08 -0500

    feat: added validation to main function

--- src/app.py
+++ src/app.py
@@ -6,7 +6,11 @@ from utils import load_cli_args
 
 
 def main() -> None:
-    num_list, expected_result = load_cli_args(sys.argv)
+    try:
+        num_list, expected_result = load_cli_args(sys.argv)
+    except:
+        print("Input arguments are incorrect. Please read README.md")
+        sys.exit(1)
     for a, b in two_sum(num_list, expected_result):
         print(f"{a}, {b}")
 
